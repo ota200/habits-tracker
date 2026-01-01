985da97bdb1c3848d9ff53135307932a9d17e43d
O.T.A <aniscratch@gmail.com>
2026-01-01 13:42:14 -0500

    more build again

--- hobby-dashboard/vite.config.js
+++ hobby-dashboard/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://ota200.github.io/habits-tracker/",
+  base: "/habits-tracker/",
   plugins: [react()],
 })
